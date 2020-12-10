1f333a15896120f9d43e013cf263e38037f7f490
Gergely Brautigam <gergely@arangodb.com>
2020-12-10 17:34:24 +0100

    A lot simpler part1

--- days/day10/part1/main.go
+++ days/day10/part1/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 1 {
+	if len(os.Args) < 2 {
 		log.Fatal("Missing file name argument.")
 	}
 	name := os.Args[1]
@@ -23,7 +23,7 @@ func main() {
 		lines = append(lines, l)
 	}
 
-	ints := make([]int, 0)
+	ints := []int{0}
 	max := 0
 	for _, l := range lines {
 		v, _ := strconv.Atoi(l)
@@ -33,34 +33,15 @@ func main() {
 		}
 	}
 
-	rating := max + 3
-
-	// pick +1 -> try if every adapter is used, then pick +2 try again... pick +3 try again...
-	// backtracking.
 	sort.Ints(ints)
 	threeDiffs := 1 // starts at one because of max+3
 	oneDiffs := 0
 
 	for i := 0; i < len(ints)-1; i++ {
-		if i == 0 && ints[i] == 1 {
-			oneDiffs++
-		}
-
 		if ints[i]+1 == ints[i+1] {
 			oneDiffs++
-			continue
-		}
-		if ints[i]+2 == ints[i+1] {
-			continue
-		}
-		if ints[i]+3 == ints[i+1] {
+		} else if ints[i]+3 == ints[i+1] {
 			threeDiffs++
-			continue
-		}
-
-		if ints[i] >= rating {
-			fmt.Println("we reached our max adapter")
-			break
 		}
 	}
 
